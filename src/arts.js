0f5bc83335a3f164b778c1ea5c215df303672388
fcduarte <fcduarte26@gmail.com>
2021-05-25 22:04:17 -0300

    Added Cards component and 3 art examples loaded from public folder

---
+++ src/arts.js
@@ -0,0 +1,23 @@
+export const arts = [
+    {
+        id: 0,
+        name: 'Jane Doe',
+        username: 'Jane123',
+        email: 'janedoe@gmail.com',
+        artfilepath: '/assets/images/img1.png'
+    },
+    {
+        id: 1,
+        name: 'Doe Jane',
+        username: 'Doe123',
+        email: 'doejane@gmail.com',
+        artfilepath: '/assets/images/img2.jpg'
+    },
+    {
+        id: 2,
+        name: 'JD',
+        username: 'JD123',
+        email: 'jd@gmail.com',
+        artfilepath: '/assets/images/img3.png'
+    },
+]
--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Card from './Card'
 import reportWebVitals from './reportWebVitals';
+import 'tachyons';
+import { arts } from './arts.js'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Card name={arts[0].name} email={arts[0].email} imgfile={arts[0].artfilepath} />
+    <Card name={arts[1].name} email={arts[1].email} imgfile={arts[1].artfilepath} />
+    <Card name={arts[2].name} email={arts[2].email} imgfile={arts[2].artfilepath} />
   </React.StrictMode>,
   document.getElementById('root')
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
